42dd9b73434d6c2183232c5ab725e530d9f12158
Sonny Wirawan <sonny.wirawan33a@gmail.com>
2022-08-07 22:40:52 +0700

    validating DashboardPostController@store using validator with error handling

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,8 @@ use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Validation\ValidationException;
+use Validator;
 
 class DashboardPostController extends Controller
 {
@@ -44,22 +46,41 @@ class DashboardPostController extends Controller
     public function store(Request $request)
     {
         // $request->file('image')->store('post-images'); --> just for testing
-        $validatedData = $request->validate([
+        // $validatedData = $request->validate([
+        //     'title' => 'required|max:255',
+        //     'slug' => 'required|unique:posts',
+        //     'category_id' => 'required',
+        //     'image' => 'image|file|max:1024',
+        //     'body' => 'required'
+        // ]);
+        $validator = Validator::make($request->all(),[
             'title' => 'required|max:255',
             'slug' => 'required|unique:posts',
             'category_id' => 'required',
-            'image' => 'image|file|max:1024',
-            'body' => 'required'
+            'body' => 'required',
+            'image' => 'required|mimes:jpeg,jpg,png,bmp,tiff|max:1024',
         ]);
-
-        if ($request->file('image')) {
-            $validatedData['image'] = $request->file('image')->store('post-images');
+        
+        // If there's error
+        if(count($validator->errors()->toArray()) > 0) {
+            $error_message = $validator->errors()->toArray();
+            $error = ValidationException::withMessages($error_message);
+            throw $error;
         }
 
-        $validatedData['user_id'] = auth()->user()->id;
-        $validatedData['excerpt'] = Str::limit(strip_tags($request->body, 200));
+        if ($request->file('image')) {
+            $request->image = $request->file('image')->store('post-images');
+        }
 
-        Post::create($validatedData);
+        // Add field user_id & excerpt to request
+        $request->request->add([
+            'user_id' => auth()->user()->id,
+            'excerpt' => Str::limit(strip_tags($request->body, 200))
+        ]);
+        // $request->user_id = auth()->user()->id;
+        // $request->excerpt = Str::limit(strip_tags($request->body, 200));
+
+        Post::create($request->all());
 
         return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
